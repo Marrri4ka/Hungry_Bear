f487552ee911d275e1b1a978fa2883411d712da1
Guest User <Guest@epicodus-79.local>
2019-06-03 10:48:11 -0700

    add feed buttons

--- src/hungrybear.js
+++ src/hungrybear.js
@@ -4,22 +4,28 @@ export class HungryBear {
   constructor(name)
   {
     this.name = name;
-    this.foodLevel = 10;
+    this.foodLevel = 100;
+    this.sleepLevel = 100;
+    this.playLevel = 100;
   }
 
   setHunger(){
 
     setInterval(()=>{
-      if(this.foodLevel>0)
+      if(this.foodLevel>0 && this.sleepLevel>0 && this.playLevel>0)
       {
         this.foodLevel --;
+        this.sleepLevel --;
+        this.playLevel --;
         $("#foodlevel").text(this.foodLevel);
+        $("#sleeplevel").text(this.sleepLevel);
+        $("#playlevel").text(this.playLevel);
         if(this.didYouGetEaten()){
           $("#hunger").text("I am hungry bear! feed me! ");
         }
       }
 
-    },100);
+    },1000);
 
   }
 
@@ -31,7 +37,15 @@ export class HungryBear {
       return true;
     }
   }
+
   feed(){
-    this.foodLevel = 10;
+    this.foodLevel +=10;
+  }
+  play(){
+    this.playLevel +=10;
+  }
+
+  sleep(){
+    this.sleepLevel +=10;
   }
 }
--- src/main.js
+++ src/main.js
@@ -4,17 +4,38 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { HungryBear } from './hungrybear.js';
 
+
+
 $(document).ready(function(){
 
+
   $("#bear").submit(function(){
+    let bear1;
     event.preventDefault();
     let bear = $('#name').val();
     $('#bearName').text(bear);
     $("#play").click(function(){
-      let bear1 = new HungryBear(bear);
+      bear1 = new HungryBear(bear);
       bear1.setHunger();
       $("#foodlevel").text(bear1.foodLevel);
 
     });
+    $("#feed").click(function(){
+      bear1.feed();
+      $("#foodlevel").text(bear1.foodLevel);
+  });
+  $("#sleep").click(function(){
+    bear1.sleep();
+    $("#sleeplevel").text(bear1.sleepLevel);
+
+  });
+
+  $("#playbutton").click(function(){
+    bear1.play();
+    $("#playlevel").text(bear1.playLevel);
+
+  });
+
+  $("#picture").append("<img src=" + picgg + ">");
   });
 });
